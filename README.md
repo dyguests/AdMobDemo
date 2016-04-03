948865c8d24adda656027b7e8f9a4f19c81051e2
fanhl <Dyguest0315>
2016-04-03 09:22:29 +0800

    add ad

---
+++ README.md
@@ -0,0 +1,7 @@
+#AdMobDemo
+
+A demo for add Google Ads.
+
+See [Android 版移动广告 SDK](https://developers.google.com/admob/android/quick-start).
+
+![example](capture/device-2016-04-03-091945.png)
--- app/src/main/java/com/fanhl/admobdemo/MainActivity.java
+++ app/src/main/java/com/fanhl/admobdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.fanhl.admobdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initAd();
+    }
+
+    private void initAd() {
+        AdView    mAdView   = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        if (mAdView != null) {
+            mAdView.loadAd(adRequest);
+        }
     }
 }
